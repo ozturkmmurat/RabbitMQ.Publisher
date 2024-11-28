c3ea00d15f15e3d51aa6df7fc8219451dc37a758
ozturkmmurat <murat.oztrkk01@gmail.com>
2024-11-29 02:52:13 +0300

    Message Durability -> RabbitMq da çökmeler veya bağlantı gibi sorunlar yaşanabilir bu durumlarda kuyruk ve kuyurktaki mesajları kaybedebiliriz bunu önlemek için kuyruk ve mesajları kalıcı olarak işaretlemeyi sağlar
    NOT:  Bu ayar yeterli olmayabilir.

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -12,18 +12,24 @@ using IModel channel = connection.CreateModel();
 
 
 //Queue Olusturma
-channel.QueueDeclare(queue:"example-queue", exclusive:false);
+//Durable true vererek kuyruğu kalıcı olarak işaretledik
+channel.QueueDeclare(queue:"example-queue", exclusive:false, durable:true);
 
 //Queue'ya Mesaj Gonderme
 
 //RabbitMQ kuyruğa gönderilecek mesajları byte türünden kabul etmektedir. Haliyle mesajları bizim byte dönüştürmemiz gerekmektedir.
 
+//Kuyruktaki mesajı kalıcı hale getiriyoruz.
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
 //Herhangi bir exchange belirtmeidgimiz icin rabbitmq direcktExchange kullanacaktir.
 for (int i = 0; i < 100; i++)
 {
     await Task.Delay(200);
-    byte[] message = Encoding.UTF8.GetBytes("Merhaba RabbitMQ" + İ);
-    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba RabbitMQ" + i);
+    //BasicProperties ile kuyruktaki mesajı kalıcı hale getirdik.
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message, basicProperties:properties);
 }
 
 Console.Read();
