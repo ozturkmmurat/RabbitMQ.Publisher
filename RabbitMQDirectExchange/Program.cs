127adafe209b231bd8d45874403d2e1cd5f325f9
ozturkmmurat <murat.oztrkk01@gmail.com>
2024-12-16 16:37:23 +0300

    FanoutExchange uygulandı

---
+++ RabbitMQDirectExchange/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//6 DERS
+
+ConnectionFactory factory = new ConnectionFactory();
+//Baglanti Olusturma
+factory.Uri = new("amqps://mbnjciig:pyWEy4hPFEOr0FY7lEe1SmJos7Emj7MB@toad.rmq.cloudamqp.com/mbnjciig");
+
+//Baglanti Aktiflestirme ve Kanal Acma
+using IConnection connection = factory.CreateConnection();
+//Kanal olusturma
+using IModel channel = connection.CreateModel();
+
+// 1 Parametre exchange adı ikinci parametre exchange tipi, tipini Direct Exchange olarak ayarladik
+// Burada exchange oluşturuyoruz
+
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Mesaj : ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    //Basic Publish mesajı yayınlıyan metod
+    //1 Parametre de hangi exchange kullanacağını bildiriyoruz
+    //2 Parametre de Mesajların bir exchange üzerinden doğru kuyruğa yönlendirilmesini sağlayan bir anahtardır.
+    //3 Parametre gönderilecek mesaj
+    channel.BasicPublish(exchange: "direct-exchange-example", routingKey: "direct-exchange-example", body: byteMessage);
+}
---
+++ RabbitMQFanoutExchange/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+//Baglanti Olusturma
+factory.Uri = new("amqps://mbnjciig:pyWEy4hPFEOr0FY7lEe1SmJos7Emj7MB@toad.rmq.cloudamqp.com/mbnjciig");
+
+//Baglanti Aktiflestirme ve Kanal Acma
+using IConnection connection = factory.CreateConnection();
+//Kanal olusturma
+using IModel channel = connection.CreateModel();
+
+//Exchange olusturuyoruz
+
+//1 Parametre exchange adi
+//2 Parametre exchange tipi
+channel.ExchangeDeclare(
+    exchange: "fanout-exchange-example",
+    type: ExchangeType.Fanout);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+
+    //Burada routingKey null verme sebebimiz FanoutExchange zaten kendine baglanmis olan yani bind olmus olan tum kuyruklara
+    // Isim gozetmeksizin deger gonderecek oldugu icin routingKey adi bos bırakilir.
+    channel.BasicPublish(
+        exchange: "fanout-exchange-example",
+        routingKey: string.Empty,
+        body: message);
+}
+
+Console.Read();
